eb295c8894adc4c8b51c61167fa358dda6439b95
Covac <filip.pazitka@gmail.com>
2021-11-12 04:54:31 +0100

    Update YoutubeRFR.py

--- YoutubeRFR.py
+++ YoutubeRFR.py
@@ -2,20 +2,21 @@ import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import asyncio
+import json
+import re
 
-
-async def YTS(textToSearch, fastsearch=True):
+async def YTS(textToSearch):
     textToSearch = str(textToSearch)
     query = urllib.parse.quote(textToSearch)
     url = "https://www.youtube.com/results?search_query=" + query
     response = urllib.request.urlopen(url)
     html = response.read()
-    if fastsearch:#FUCKING WORTH!!!
-        hlen = len(html)
-        slicer1 = hlen // 3
-        slicer2 = hlen // 2 #50% found 25% not found 30% not found
-        html = html[slicer1:slicer2]
     #await asyncio.sleep(0.1)
     soup = BeautifulSoup(html, "html.parser")
-    links = soup.findAll(attrs={'class':'yt-uix-tile-link'})
-    return 'https://www.youtube.com' + links[0]['href']
+    print(soup)
+    links = soup.find_all('script',text=re.compile('var ytInitialData ='))
+    data = re.search(r'ytInitialData = ({.*?});',
+                      str(links), flags=re.DOTALL | re.MULTILINE).group(1)
+    data = json.loads(data)
+    url = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][0]["videoRenderer"]["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]
+    return 'https://www.youtube.com' + url
