d2d2965f24a5c25eb2b8c088b531514c03240751
Covac <filip.pazitka@gmail.com>
2021-11-13 04:16:37 +0100

    Update newpythonbot.py

--- newpythonbot.py
+++ newpythonbot.py
@@ -64,11 +64,11 @@ class MyClient(discord.Client):
             print('Message from {0.author} on {0.guild}: {0.content}'.format(message))
             #guildid = message.guild.id
             
-        if message.content.startswith('!!help'):
-            await message.channel.send('!!play <LINK> play or queue it OR !!play <TEXT> returns first result from YT\n!!skip skips :)')
+        if message.content.startswith('!help'):
+            await message.channel.send('!play <LINK> play or queue it OR !play <TEXT> returns first result from YT\n!skip skips :)')
             
-        if message.content.startswith('!!play'):
-            link = message.content.strip('!!play')
+        if message.content.startswith('!play'):
+            link = message.content.strip('!play')
             if (not('http' in link) or not('www.' in link) or not('.com' in link)):
                 try:#LAZY
                     link = await YTS(link)#maybe test it without await after lag fix
@@ -112,7 +112,7 @@ class MyClient(discord.Client):
                     await self.guildVCs[guild].disconnect()
                     del self.guildQ[guild]
 
-        if message.content.startswith('!!skip'):
+        if message.content.startswith('!skip'):
             if self.guildVCs[guild].is_playing():
                 self.guildVCs[guild].stop()
                 
